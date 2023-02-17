b229b673c248abe408427e687d719e40d982ebf2
nobu09 <nobu0922@gmail.com>
2023-02-17 23:51:24 +0900

    StorybookのTheme設定 fontSizesを実装中

---
+++ src/themes/fontSize.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const fontSizes: any = [12, 14, 16, 20, 24, 32];
+
+// aliases
+fontSizes.extraSmall = fontSizes[0];
+fontSizes.small = fontSizes[1];
+fontSizes.medium = fontSizes[2];
+fontSizes.large = fontSizes[3];
+fontSizes.extraLarge = fontSizes[4];
