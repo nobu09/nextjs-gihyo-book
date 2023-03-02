a33c360deb835e8915ac5e4b676197b3890ca680
nobu09 <nobu0922@gmail.com>
2023-03-02 09:10:34 +0900

    ユーザー取得APIを実装

--- src/services/users/get-user.ts
+++ src/services/users/get-user.ts
@@ -7,3 +7,38 @@ export type GetUserParamas = {
    */
   id: number
 }
+
+/**
+ * ユーザーAPI（個別取得）
+ * @param context APIコンテキスト
+ * @param params パラメータ
+ * @returns ユーザー
+ */
+const getUser = async (
+  context: ApiContext,
+  { id }: GetUserParams
+): Promise<User> => {
+  /**
+  // ユーザーAPI
+  // サンプルレスポンス
+  {
+    "id": "1",
+    "username": "taketo",
+    "displayName": "Taketo Yoshida",
+    "email": "taketo@example.com",
+    "profileImageUrl": "/users/1.png",
+    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting indestry"
+  }
+  */
+  return await fetcher(
+    `${context.apiRootUrl.replace(/\/$/g, '')}/users/${id}`,
+    {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+}
+
+export default getUser
