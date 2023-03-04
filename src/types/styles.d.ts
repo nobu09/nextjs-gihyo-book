b519fc0ea167b74e31202c26de85b804eecbf549
nobu09 <nobu0922@gmail.com>
2023-03-05 08:48:34 +0900

    レスポンシブデザインをstyled-componentで実現するために、ブレークポイントごとにCSSプロパティの値を設定できるResponsive型を定義した

---
+++ src/types/styles.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Responsive プロパティ
+ * CSS プロパティの値をブレークポイントごとに設定できる
+ * T はCSS プロパティの値の型
+ */
+export type ResponsiveProp<T> = {
+  base?: T // デフォルト
+  sm?: T // 640px 以上
+  md?: T // 768px 以上
+  lg?: T // 1024px 以上
+  xl?: T // 1280px 以上
+}
+
+/**
+ * Responsive 型は Responsive プロパティ もしくは CSS プロパティの値
+ */
+export type Responsive<T> = T | ResponsiveProp<T>
