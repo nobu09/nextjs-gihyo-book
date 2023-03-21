9f5b1914dc1b8225df29508136bd87c79e585872
nobu09 <nobu0922@gmail.com>
2023-03-21 11:43:43 +0900

    Boxコンポーネントの作成中

--- src/components/layout/Box/index.tsx
+++ src/components/layout/Box/index.tsx
@@ -4,4 +4,33 @@ import type { Responsive } from 'types/styles'
 import { toPropValue, Color, Space } from 'utils/styles'
 
 // Box がとりうるプロパティを列挙
-export type BoxProps = {}
+export type BoxProps = {
+  color?: Responsive<Color>
+  backgroudColor?: Responsive<Color>
+  width?: Responsive<string>
+  height?: Responsive<string>
+  minWidth?: Responsive<string>
+  minHeight?: Responsive<string>
+  display?: Responsive<string>
+  border?: Responsive<string>
+  overflow?: Responsive<string>
+  margin?: Responsive<Space>
+  marginTop?: Responsive<Space>
+  marginRight?: Responsive<Space>
+  marginBotoom?: Responsive<Space>
+  marginLeft?: Responsive<Space>
+  padding?: Responsive<Space>
+  paddingTop?: Responsive<Space>
+  paddingRight?: Responsive<Space>
+  paddingBottom?: Responsive<Space>
+  paddingLeft?: Responsive<Space>
+}
+
+/**
+ * Box コンポーネント
+ * レイアウトの調整に利用する
+ * ${(props) => toPropValue('color', props.color, props.theme)}
+ */
+const Box = styled.div<BoxProps>``
+
+export default Box
