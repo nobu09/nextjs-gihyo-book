db8fb7a8a58d8a3e387e442f956195b912e6a9b2
nobu09 <nobu0922@gmail.com>
2023-03-23 06:58:46 +0900

    Boxコンポーネントの実装完了

--- src/components/layout/Box/index.tsx
+++ src/components/layout/Box/index.tsx
@@ -17,7 +17,7 @@ export type BoxProps = {
   margin?: Responsive<Space>
   marginTop?: Responsive<Space>
   marginRight?: Responsive<Space>
-  marginBotoom?: Responsive<Space>
+  marginBottom?: Responsive<Space>
   marginLeft?: Responsive<Space>
   padding?: Responsive<Space>
   paddingTop?: Responsive<Space>
@@ -45,6 +45,13 @@ const Box = styled.div<BoxProps>`
   ${(props) => toPropValue('margin', props.margin, props.theme)}
   ${(props) => toPropValue('margin-top', props.marginTop, props.theme)}
   ${(props) => toPropValue('margin-left', props.marginLeft, props.theme)}
+  ${(props) => toPropValue('margin-bottom', props.marginBottom, props.theme)}
+  ${(props) => toPropValue('margin-right', props.marginRight, props.theme)}
+  ${(props) => toPropValue('padding', props.padding, props.theme)}
+  ${(props) => toPropValue('padding-top', props.paddingTop, props.theme)}
+  ${(props) => toPropValue('padding-left', props.paddingLeft, props.theme)}
+  ${(props) => toPropValue('padding-bottom', props.paddingBottom, props.theme)}
+  ${(props) => toPropValue('padding-right', props.paddingRight, props.theme)}
 `
 
 export default Box
