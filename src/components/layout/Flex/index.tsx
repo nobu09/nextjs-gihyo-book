3fc48a5aa064f685721dd2c65248f26fc091a0c9
nobu09 <nobu0922@gmail.com>
2023-03-24 09:30:24 +0900

    Flexコンポーネントを実装中

--- src/components/layout/Flex/index.tsx
+++ src/components/layout/Flex/index.tsx
@@ -1,4 +1,23 @@
 /* eslint-disable prettier/prettier */
 import styled from 'styled-components'
-import type { Responsive } from 'types/styles'
+import type {
+  Responsive,
+  CSSPropertyAlignItems,
+  CSSPropertyAlignContent,
+  CSSPropertyJustifyContent,
+  CSSPropertyJustifyItems,
+  CSSPropertyFlexDirection,
+  CSSPropertyJustifySelf,
+  CSSPropertyFlexWrap,
+  CSSPropertyAlignSelf,
+} from 'types/styles'
 import { toPropValue } from 'utils/styles'
+
+type FlexProps = BoxProps & {
+  alignItems?: Responsive<CSSPropertyAlignItems>
+  alignContent?: Responsive<CSSPropertyAlignContent>
+  justifyContent?: Responsive<CSSPropertyJustifyContent>
+  justifyItems?: Responsive<CSSPropertyJustifyItems>
+  flexWrap?: Responsive<CSSPropertyFlexWrap>
+  flexBasis?: Responsive<string>
+}
