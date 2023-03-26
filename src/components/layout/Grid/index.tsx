4b2dd56d5077df3eec496bf42d4fd14d80e9e404
nobu09 <nobu0922@gmail.com>
2023-03-27 07:13:52 +0900

    Gridコンポーネントを実装し始め

---
+++ src/components/layout/Grid/index.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import styled from 'styled-components'
+import Box, { BoxProps } from 'components/layout/Box'
+import type {
+  CSSPropertyGridArea,
+  CSSPropertyGridAutoFlow,
+  CSSPropertyGridColumn,
+  CSSPropertyGridRow,
+  Responsive,
+} from 'types/styles'
+import { toPropValue } from 'utils/styles'
+
+type GridProps = BoxProps & {
+  gridGap?: Responsive<string>
+  gridColumnGap?: Responsive<string>
+  gridRowGap?: Responsive<string>
+  gridColumn?: Responsive<CSSPropertyGridColumn>
+  gridRow?: Responsive<CSSPropertyGridRow>
+  gridAutoFlow?: Responsive<CSSPropertyGridAutoFlow>
+}
