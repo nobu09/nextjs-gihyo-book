167d9602e192871ca501969ccf2fc440d7eee126
nobu09 <nobu0922@gmail.com>
2023-04-30 08:19:33 +0900

    Buttonコンポーネントを作成し始めた　Atom要素

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import styled from 'styled-components'
+import { Responsive } from 'types'
+import {
+    toPropValue,
+    Color,
+    FontSize,
+    LetterSpacings,
+    LineHeight,
+    Space,
+} from 'utils/styles'
